110a69f9c809a58ed8aa76bcdd45d997e660098c
ronaninstain <shoivehossain100@gmail.com>
2021-06-19 22:57:40 +0600

    A new Repo.

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    /* Main Slider Js */
+    $(".sliders").owlCarousel({
+        'items': 1,
+        'loop': true,
+        'autoplay': true
+    });    
+
+  });
