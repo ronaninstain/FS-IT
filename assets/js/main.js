8c1995387d649c1e50737ff7ebac86c59f4af84d
fabihaMU47 <fabihasyeda084@gmail.com>
2021-07-11 14:40:46 +0600

    Testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,15 @@ $(document).ready(function(){
         'loop': true,
         'autoplay': true
     }); 
+
+    /* Testimonial Active */
+    $(".testimonials").owlCarousel({
+        'items': 3,
+        'loop': true,
+        'autoplay': true,
+        'margin': 30,
+        'center':true
+    }); 
     
     /* CounterUp Active */
     $('.counter').counterUp();
